9c9b809bb1ef3c3be18ea037f9ad99796713caa6
leoSallos <sallosl@oregonstate.edu>
2025-11-21 18:18:50 -0800

    created list get list of notes

--- server.js
+++ server.js
@@ -31,12 +31,61 @@ app.use(function (req, res, next) {
 //
 
 // server ping
-app.get("/", function(req, res, next) {
+app.get("/", function(req, res) {
     console.log("Ping recieved")
     res.status(204);
 });
 
+// get note list
+app.get("/list/:userID", async function(req, res){
+    console.log("Getting user list about user.");
 
+    // check if user exists
+    const userID = req.params.userID;
+    var path = __dirname + "/data/" + userID + ".json";
+    try {
+        var userDataString = await fs.readFile(path, "utf8");
+    } catch (err) {
+        console.error("User data could not be retrieved.");
+        res.status(404).send("User data not found.");
+        return;
+    }
+
+    // get all notifications
+    if (userDataString != ""){
+        var userData = JSON.parse(userDataString);
+    } else {
+        console.log("No user notes found.");
+        res.status(204).send("User has no notes.");
+        return;
+    }
+
+    // create list
+    var notesList = {noteList: []};
+    for (var i = 0; i < userData.notes.length; i++){
+        const note = {
+            id: i, 
+            title: userData.notes[i].title
+        };
+        notesList.noteList.push(note);
+    }
+
+    // send to user
+    console.log("Sending success");
+    res.status(200).json(notesLists);
+});
+
+// get note
+app.get("/get/:userID/:noteID", function(req, res){
+});
+
+// post new note
+app.post("/new/:userID/", function(req, res){
+});
+
+// post note update
+app.post("/update/:userID/:noteID", function(req, res){
+});
 
 // start server listening
 app.listen(port, function(err){
